8f0c89209941eddd1df656d36fad72138e26d9ce
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-02-03 20:25:00 -0600

    dependecie

--- APISalesSystem/SaleSystem.IOC/Dependencie.cs
+++ APISalesSystem/SaleSystem.IOC/Dependencie.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SaleSystem.DAL.DBContext;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +10,14 @@ using System.Threading.Tasks;
 
 namespace SaleSystem.IOC
 {
-    // Path: SaleSystem.IOC\Dependencie.cs
-    internal class Dependencie
+    public static class Dependencie
     {
+        public static void Injectdependencies(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DbventaContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("stringSQL"));
+            });
+        }
     }
 }
