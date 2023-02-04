a9a4d33bee1612c1bf49d4d575590b8b0ad611e7
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-02-03 20:54:17 -0600

    create

--- APISalesSystem/SaleSystem.DAL/Repository/GenericRepository.cs
+++ APISalesSystem/SaleSystem.DAL/Repository/GenericRepository.cs
@@ -3,10 +3,83 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SaleSystem.DAL.Repository.contract;
+using SaleSystem.DAL.DBContext;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace SaleSystem.DAL.Repository
 {
-    internal class GenericRepository
+    public class GenericRepository<TModel> : IGenericRepository<TModel> where TModel : class
     {
+        private readonly DbContext _dbcontext;
+
+        public GenericRepository(DbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+        public async Task<TModel> Create(TModel model)
+        {
+            try
+            {
+                _dbcontext.Set<TModel>().Add(model);
+                await _dbcontext.SaveChangesAsync();
+                return model;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public Task<bool> Delete(TModel model)
+        {
+            try
+            {
+
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public async Task<TModel> Get(Expression<Func<TModel, bool>> filter)
+        {
+            try
+            {
+                TModel model = await _dbcontext.Set<TModel>().FirstOrDefaultAsync(filter);
+                return model;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public Task<IQueryable<TModel>> Query(Expression<Func<TModel, bool>> filter = null)
+        {
+            try
+            {
+
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public Task<bool> Update(TModel model)
+        {
+            try
+            {
+
+            }
+            catch
+            {
+                throw;
+            }
+        }
     }
 }
