254ec982e6f8c66509ed6aca69747facaf234236
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-02-03 20:53:12 -0600

    get

---
+++ APISalesSystem/SaleSystem.DAL/Repository/contract/ISaleRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SaleSystem.Model;
+
+namespace SaleSystem.DAL.Repository.contract
+{
+    public interface ISaleRepository : IGenericRepository<Venta>
+    {
+        Task<Venta> Register(Venta model);
+        
+    }
+}
