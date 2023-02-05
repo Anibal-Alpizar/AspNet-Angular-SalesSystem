85ac3d3c928ecb8612f83ec839a156a43fe42956
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-02-04 22:27:18 -0600

    Rol AutoMapper

---
+++ APISalesSystem/SaleSystem.Utility/AutoMapperProfile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using SaleSystem.DTO;
+using SaleSystem.Model;
+
+namespace SaleSystem.Utility
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            #region Rol
+            CreateMap<Rol, RolDTO>().ReverseMap();                    
+            #endregion
+        }
+    }
+}
