46f0856f18a5d2b842b99d058f9ff16e85181338
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-02-04 22:10:53 -0600

    All DTO model for work in Angular

---
+++ APISalesSystem/SaleSystem.DTO/DashBoardDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaleSystem.DTO
+{
+    public class DashBoardDTO
+    {
+        public int TotalVentas { get; set; }
+
+        public int TotalIngresos { get; set; }
+
+        public List<VentasSemanaDTO> VentasUltimaSemana { get; set; }
+
+    }
+}
