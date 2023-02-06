0cfe47618ab0eefd406b44562a0a2dce9531e931
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-02-06 09:49:25 -0600

    Rol mapper

---
+++ APISalesSystem/SaleSystem.BLL/Services/Contract/IRolService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SaleSystem.DTO;
+
+namespace SaleSystem.BLL.Services.Contract
+{
+    public interface IRolService
+    {
+        Task<List<RolDTO>> List(); 
+    }
+}
