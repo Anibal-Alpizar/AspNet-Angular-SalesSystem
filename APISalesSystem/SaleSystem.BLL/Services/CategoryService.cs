142a0698b4014e10b4ecf6f0bb332eee015cc361
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-02-06 17:53:30 -0600

    List from CategoryDTO

---
+++ APISalesSystem/SaleSystem.BLL/Services/CategoryService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using SaleSystem.BLL.Services.Contract;
+using SaleSystem.DAL.Repository.contract;
+using SaleSystem.DTO;
+using SaleSystem.Model;
+
+namespace SaleSystem.BLL.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly IGenericRepository<Categoria> _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public CategoryService(IGenericRepository<Categoria> categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<CategoriaDTO>> List()
+        {
+            try
+            {
+                var categoryList = await _categoryRepository.Query();
+                return _mapper.Map<List<CategoriaDTO>>(categoryList.ToList());
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
