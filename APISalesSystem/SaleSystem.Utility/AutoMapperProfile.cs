2045999066a11d86c673a8c7f9b71bf96a61ec8c
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-02-05 22:32:57 -0600

    all mappers ready

--- APISalesSystem/SaleSystem.Utility/AutoMapperProfile.cs
+++ APISalesSystem/SaleSystem.Utility/AutoMapperProfile.cs
@@ -14,8 +14,38 @@ namespace SaleSystem.Utility
         public AutoMapperProfile()
         {
             #region Rol
-            CreateMap<Rol, RolDTO>().ReverseMap();                    
-            #endregion
+            CreateMap<Rol, RolDTO>().ReverseMap();
+            #endregion Rol
+
+            #region Menu
+            CreateMap<Menu, MenuDTO>().ReverseMap();
+            #endregion Menu
+
+            #region Usuario
+            CreateMap<Usuario, UsuarioDTO>()
+            .ForMember(dest =>
+            dest.RolDescripcion, opt => opt.MapFrom(orgin => orgin.IdRolNavigation.Nombre)
+            )
+            .ForMember(dest => dest.EsActivo,
+            opt => opt.MapFrom(orgin => orgin.EsActivo == true ? 1 : 0));
+
+            CreateMap<Usuario, SesionDTO>()
+            .ForMember(dest =>
+            dest.RolDescripcion,
+            opt => opt.MapFrom(orgin => orgin.IdRolNavigation.Nombre)
+            );
+
+            CreateMap<UsuarioDTO, Usuario>()
+            .ForMember(dest => dest.IdRolNavigation,
+            opt => opt.Ignore())
+            .ForMember(dest => dest.EsActivo,
+            opt => opt.MapFrom(orgin => orgin.EsActivo == 1 ? true: false));
+
+            #endregion Usuario
+
+            #region Categoria
+            CreateMap<Categoria, CategoriaDTO>().ReverseMap();
+            #endregion Categoria
         }
     }
 }
